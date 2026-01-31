faf36c5f1322a51a082d17e2d5c6d0e59b13a981
Shune Lai Wai <shunelaiwai@Shunes-Laptop.local>
2026-01-31 21:42:18 +0700

    second commit

--- src/app/lib/cors.js
+++ src/app/lib/cors.js
@@ -1,7 +1,7 @@
 let corsHeaders = {
     "Access-Control-Allow-Origin": "*"
     ,
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Max-Age": "86400",
 };
