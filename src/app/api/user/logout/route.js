d05e8564fe87b6d8a272a1d093a700a1097f6b93
Shune <shunelaiw@gmail.com>
2026-02-21 21:35:13 +0700

    defg

--- src/app/api/user/logout/route.js
+++ src/app/api/user/logout/route.js
@@ -1,4 +1,4 @@
-import corsHeaders from "@/lib/cors";
+import corsHeaders from "@/app/lib/cors";
 import { NextResponse } from "next/server";
 export async function OPTIONS(req) {
     return new Response(null, {
